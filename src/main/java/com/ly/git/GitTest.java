77b7bb970ab290e1edc4880ec4404c1b3fffeb24
liuliu-creator <ly3034ly@qq.com>
2025-11-17 15:23:53 +0800

    pull commit

--- src/main/java/com/ly/git/GitTest.java
+++ src/main/java/com/ly/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("hot-fix test");
         System.out.println("hot-fix push test");
         System.out.println("hot-fix push ssh test");
+         System.out.println("hot-fix pull test");
     }
 }
